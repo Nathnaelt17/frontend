eab6ceb1a8d586104c0b4447b5c51f40943fa59e
Nathnaelt17 <Nathnaelt17@gmail.com>
2026-06-24 18:52:57 +0300

    loginn page fi

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -129,9 +129,9 @@ export function LoginPage() {
               />
             </div>
 
-            <Button
+            <Button className="w-full mt-4 bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
               type="submit"
-              className="w-full"
+              
               disabled={loading}
             >
               {loading
